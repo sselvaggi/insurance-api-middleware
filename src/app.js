78207c620d1795cd8143a916b7dba22c24da567c
sselvaggi <sergio.selvaggi@gmail.com>
2020-11-03 09:01:08 +0100

    added swagger rest client and default index.html

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const path = require('path');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./openapi.json');
 
 require('dotenv').config();
 
@@ -9,17 +12,13 @@ const middlewares = require('./middlewares');
 const api = require('./api');
 
 const app = express();
+app.use('/api/v1', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
-  });
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1', api);
 
